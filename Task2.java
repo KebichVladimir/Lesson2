c13a8db4b3cdfdced45120398cf82bb72e832b5f
Kebich Vladimir <friend7by@gmail.com>
2018-02-23 23:15:10 +0300

    Task2

---
+++ Task2.java
@@ -0,0 +1,10 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 237;
+        if (a % 10 == 7) {
+            System.out.println("Последняя цифра числа " + a + " является 7");
+        } else {
+            System.out.println("Последняя цифра числа " + a + " не является 7");
+        }
+    }
+}
